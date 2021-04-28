e6df1a7ea613ab3bb26ef872ac5725476f1e9c15
Brian Scally <scallybrian@gmail.com>
2021-04-28 18:44:26 +0100

    Upload some models, add docs, add source, add ref for models to build DAG

---
+++ models/example/scaffold.sql
@@ -0,0 +1,57 @@
+{{ config(materialized='view', transient=false) }}
+
+WITH
+	-- Select intial fields to take forward
+	init_table
+	AS
+	(
+		SELECT
+			customer_id,
+			sub_category AS product,
+			invoice_start,
+			invoice_end,
+			SUM(sales) AS sales
+		FROM
+			{{ source('snowball', "SNOWBALL_INVOICES") }}
+
+		GROUP BY customer_id,
+			sub_category,
+			invoice_start,
+			invoice_end
+	),
+
+	-- Generate rows for monthly dataset
+	data_monthly
+	AS
+	(
+			SELECT customer_id,
+				product,
+				invoice_start as month,
+				invoice_end,
+				sales,
+				invoice_start
+			-- duplicate to save in dataset
+			FROM
+				init_table
+			GROUP BY customer_id,
+				product,
+				invoice_start,
+				invoice_end,
+				sales
+		UNION ALL
+			SELECT
+				customer_id,
+				product,
+				DATEADD(MONTH, 1, month),
+				invoice_end,
+				sales,
+				invoice_start
+			FROM
+				data_monthly
+			WHERE
+				DATEADD(MONTH, 1, month) < invoice_end
+
+
+	)
+
+SELECT * FROM data_monthly
